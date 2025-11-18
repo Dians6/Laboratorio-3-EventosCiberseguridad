a28818afcabc56a15f8a6cb3e3ac094b91f66fce
diana <diana@192.168.5.108>
2025-11-18 12:36:31 -0400

    laboratorio repositorio

---
+++ src/laboratorioevaluado/EventoIntrusion.java
@@ -0,0 +1,113 @@
+
+package laboratorioevaluado;
+
+import java.util.InputMismatchException;
+import java.time.LocalTime;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
+public class EventoIntrusion extends EventoSeguridad{
+    private String puntoAcceso;
+    private int intentosAcceso;
+    private String usuarioAfectado;
+    
+    Scanner entrada = new Scanner(System.in);
+
+    
+    public EventoIntrusion(String idEvento, String tipoAmenaza, int nivelRiesgo, LocalDate fechaDeteccion, String descripcion, String puntoAcceso, int intentosAcceso, String usuarioAfectado) {
+        super(idEvento, tipoAmenaza, nivelRiesgo, fechaDeteccion, descripcion);
+        this.puntoAcceso = puntoAcceso;
+        this.intentosAcceso = intentosAcceso;
+        this.usuarioAfectado = usuarioAfectado; 
+    }
+    
+    public EventoIntrusion() {
+        //super();
+        puntoAcceso = "N/A";
+        intentosAcceso = 0;
+        usuarioAfectado = "N/A";
+    }
+    
+    @Override
+    public void leerDatos() {
+        super.leerDatos();
+        puntoAcceso = validarCadena("Introduzca la direccion IP asignada: ","Error: IP invalido. Solo se permiten letras, números, espacios y ciertos signos de puntuación (1-250 caracteres).","^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ\\s.',-]{1,250}$");
+        intentosAcceso = validarEntero("Ingrese los intentos de acceso hacia el usuario (maximo 20): ", "Error: el numero debe ser del 1 al 20.",1,20);
+        usuarioAfectado = validarCadena("Introduzca el nombre del usuario afectado: ","Error: Titulo invalido. Solo se permiten letras, números, espacios y ciertos signos de puntuación (1-250 caracteres).","^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ\\s.',-]{1,250}$");
+    }
+    
+    public void bloquearPuntoAcceso() {
+        System.out.println("---BLOQUEANDO IP DEL USUARIO---");
+        System.out.println("!!! IP BLOQUEADA !!!");
+        
+    }
+    
+    public void aislarSistema() {
+        System.out.println("---PROTOCOLO DE AISLAMIENTO DEL SISTEMA---");
+        System.out.println("Se ha detectado una amenaza en su sistema, desea aislarlo?");
+        int decision = validarEntero("(1: Si, 2: No)","Debe ingresar un numero entre 1 y 2",1,2);
+        if (decision == 1) {
+            System.out.println("---AISLANDO SISTEMA...---");
+            System.out.println("!!! AISLADO SATISFACTORIAMENTE !!!");
+        } else {
+            System.out.println("!!! ACCION CANCELADA !!!");
+        }
+    }
+    
+    public void notificarAdministrador() {
+        System.out.println("---ANALIZANDO ALTERACIONES EN LA CUENTA DEL USUARIO...---");
+        System.out.println("NIVEL DE RIESGO HACIA EL USUARIO:"+ nivelRiesgo);
+        System.out.println("NUMERO DE INTENTOS DE ACCESO HACIA EL USUARIO:"+ intentosAcceso);
+        
+        if (nivelRiesgo == 3 && intentosAcceso >= 15) {
+            aislarSistema();
+        }
+        else {
+            System.out.println("No hay riesgo para aislar el sistema.");
+        }
+    }
+    
+    @Override
+    public void GenerarReporte(boolean eResumido){
+         super.GenerarReporte(eResumido);
+         System.out.println("Fecha de Deteccion: "+fechaDeteccion);
+         System.out.println("Direccion IP o sistema desde donde se origino la intrusion: "+puntoAcceso);
+         System.out.println("Numero de intentos de acceso no autorizados: "+intentosAcceso);
+         System.out.println("---------------------------------------------------------");
+    }
+
+    @Override
+    public void GenerarReporte(){
+         super.GenerarReporte();
+         System.out.println("Direccion IP o sistema desde donde se origino la intrusion: "+puntoAcceso);
+         System.out.println("Numero de intentos de acceso no autorizados: "+intentosAcceso);
+
+         String nivelPersis = "";
+         if ((intentosAcceso >= 1) && (intentosAcceso <= 5)){
+             nivelPersis = "Exploracion";
+         }
+         if ((intentosAcceso >= 6) && (intentosAcceso <= 15)){
+             nivelPersis = "Ataque Coordinado";
+         }
+         if (intentosAcceso > 15){
+             nivelPersis = "Amenaza Persistente Avanzada";
+         }
+         System.out.println("Nivel de persistencia basado en intentos de acceso: "+nivelPersis);
+         System.out.println("---------------------------------------------------------");
+
+    }
+   
+    @Override
+    public void analizarImpacto(){
+        super.analizarImpacto();
+        //si se superan los 10 intentos y el nivel de riesgo es alto alerta critica
+        if ((nivelRiesgo == 3) && (intentosAcceso >= 10)){
+            System.out.println("ALERTA CRITICA: Posible compromiso de sistema");
+        }
+    }
+   
+    
+}
---
+++ src/laboratorioevaluado/EventoSeguridad.java
@@ -0,0 +1,195 @@
+
+package laboratorioevaluado;
+
+import java.util.InputMismatchException;
+import java.time.LocalTime;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
+
+public class EventoSeguridad {
+    protected String idEvento;
+    protected String tipoAmenaza;
+    protected int nivelRiesgo; //1: Bajo - 2:Medio - 3: Alto
+    protected LocalDate fechaDeteccion;
+    protected String descripcion;
+    
+    public EventoSeguridad(){
+        idEvento = "N/A";
+        tipoAmenaza = "N/A";
+        nivelRiesgo = 0;
+        fechaDeteccion = null;
+        descripcion = "N/A";
+    }
+    
+    public EventoSeguridad(String idEvento, String tipoAmenaza, int nivelRiesgo, LocalDate fechaDeteccion, String descripcion) {
+        this.idEvento = idEvento ;
+        this.tipoAmenaza = tipoAmenaza;
+        this.nivelRiesgo = nivelRiesgo;
+        this.fechaDeteccion = fechaDeteccion;
+        this.descripcion = descripcion;
+    }
+
+    public String getIdEvento() {
+        return idEvento;
+    }
+
+    public void setIdEvento(String idEvento) {
+        this.idEvento = idEvento;
+    }
+
+    public String getTipoAmenaza() {
+        return tipoAmenaza;
+    }
+
+    public void setTipoAmenaza(String tipoAmenaza) {
+        this.tipoAmenaza = tipoAmenaza;
+    }
+
+    public int getNivelRiesgo() {
+        return nivelRiesgo;
+    }
+
+    public void setNivelRiesgo(int nivelRiesgo) {
+        this.nivelRiesgo = nivelRiesgo;
+    }
+
+    public LocalDate getFechaDeteccion() {
+        return fechaDeteccion;
+    }
+
+    public void setFechaDeteccion(LocalDate fechaDeteccion) {
+        this.fechaDeteccion = fechaDeteccion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    Scanner entrada = new Scanner(System.in);
+    
+    public int validarEntero(String mensaje, String error, int limiteInf, int limiteSup){
+       int valor = 0;
+       boolean continuar = true;
+        do {
+            try {
+                System.out.print(mensaje);
+                valor = entrada.nextInt();
+                if (valor < limiteInf || valor > limiteSup) {
+                    System.out.println(error);
+                } else {
+                    continuar = false;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Debes indicar un numero entero positivo.");
+                entrada.nextLine(); // Limpia el buffer del Scanner para evitar un bucle infinito
+            }
+        } while (continuar);
+        entrada.nextLine(); // Limpia el buffer después de la lectura exitosa del número
+        return valor;
+    }
+
+    protected String validarCadena(String mensaje, String error, String regex){
+        String valor;
+        boolean continuar = true;
+        do {
+            System.out.print(mensaje);
+            valor = entrada.nextLine();
+            if (valor.matches(regex)) {
+                continuar = false;
+            } else {
+                System.out.println(error);
+            }
+        } while (continuar);
+        return valor;
+    }
+    
+    protected LocalDate validarFecha(String mensaje, String mensajeError) {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/uuuu").withResolverStyle(ResolverStyle.STRICT);
+        
+        LocalDate fechaValida = null;
+        while (fechaValida == null) {
+            try {
+                System.out.print(mensaje);
+                String fechaentrada = entrada.nextLine().trim();
+                LocalDate fecha = LocalDate.parse(fechaentrada, dtf);
+                LocalDate hoy = LocalDate.now();
+            if (fecha.isBefore(hoy) || fecha.isEqual(hoy)) {
+                fechaValida = fecha;
+            } else {
+                System.out.println("Error: La fecha debe ser hasta el dia de hoy. La fecha no debe ser futura.");
+            }
+            } catch (DateTimeParseException e) {
+                System.out.println("Error: " + mensajeError);
+            }
+        }
+        return fechaValida;
+    }
+
+
+    public void leerDatos(){
+        idEvento = validarCadena("Ingrese el ID del evento: ","Error: Titulo invalido. Solo se permiten letras, números, espacios y ciertos signos de puntuación (1-250 caracteres).","^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ\\s.',-]{1,250}$");
+        tipoAmenaza = validarCadena("Ingrese el tipo de amenaza: ","Error: Titulo invalido. Solo se permiten letras, números, espacios y ciertos signos de puntuación (1-250 caracteres).","^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ\\s.',-]{1,250}$");
+        nivelRiesgo = validarEntero("Ingrese un numero del 1 al 3 segun el nivel de riesgo de la amenaza (1 = Bajo; 2 = Medio; 3 = Alto): ", "Error: el numero debe ser del 1 al 3.",1,3);
+        fechaDeteccion = validarFecha("Ingrese la fecha en que se detecto el evento (dd/MM/yyyy): ","Error: Formato incorrecto o fecha inexistente. Asegurate de usar dd/MM/yyyy");
+        descripcion = validarCadena("Ingrese la descripcion del incidente: ","Error: Titulo invalido. Solo se permiten letras, números, espacios y ciertos signos de puntuación (1-250 caracteres).","^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ\\s.',-]{1,250}$");
+        }
+    
+    public void GenerarReporte(boolean esResumido){
+        System.out.println("--GENERANDO REPORTE BASICO--");
+        System.out.println("identificador: "+idEvento);
+        System.out.println("Tipo de Amenaza: "+tipoAmenaza);
+        System.out.println("Descripcion: "+descripcion);
+    }
+    
+    public void GenerarReporte(){
+        System.out.println("--GENERANDO REPORTE DETALLADO--");
+        System.out.println("identificador: "+idEvento);
+        System.out.println("Tipo de Amenaza: "+tipoAmenaza);
+        
+        switch (nivelRiesgo){
+            case 1->System.out.println("Nivel de Riesgo: Amenaza Leve"); 
+            case 2->System.out.println("Nivel de Riesgo: Amenaza Moderada");   
+            case 3->System.out.println("Nivel de Riesgo: Amenaza Critica");
+            default->System.out.println("Nivel de Riesgo: Sin Amenaza Detectada");
+        }
+        System.out.println("Descripcion: "+descripcion);
+        System.out.println("Fecha de Deteccion: "+fechaDeteccion);
+    }
+    
+    public void analizarImpacto(){
+        switch (nivelRiesgo){
+            case 1->System.out.println("El nivel de riesgo es bajo. NO SE REQUIERE ACCION"); 
+            case 2->System.out.println("El nivel de riesgo es medio. TOME SUS PRECAUCIONES");   
+            case 3->System.out.println("El nivel de riesgo es alto. ACCION INMEDIATA REQUERIDA");
+            default->System.out.println("No hay nivel de Riesgo: Sin Amenaza Detectada");
+        }
+    }
+
+    public void mostrarInformacion(){
+        System.out.println("ID del evento: " + idEvento);
+        System.out.println("Tipo de Amenaza: " + tipoAmenaza);
+        System.out.print("Nivel de riesgo: ");
+        switch (nivelRiesgo) {
+            case 1 -> System.out.println("Bajo");
+            case 2 -> System.out.println("Medio");
+            case 3 -> System.out.println("Alto");
+            default -> System.out.println("Nivel de riesgo desconocido (" + nivelRiesgo + ")");
+            }
+        System.out.println("Fecha de deteccion: " + fechaDeteccion);
+        System.out.println("Descripcion: " + descripcion);
+    }
+
+    
+    
+    
+    
+}
