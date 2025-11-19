3f392f11002c0218f6322b77f9d7189903c76f0b
diana <diana@192.168.5.108>
2025-11-18 20:18:09 -0400

    inicie a modificar las clases

--- src/controlador/controladora.java
+++ src/controlador/controladora.java
@@ -4,8 +4,15 @@
  */
 package controlador;
 
+import java.awt.event.KeyEvent;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 
 /**
  *
@@ -20,5 +27,51 @@ public class controladora {
                                            //la ventana reemplazando la taza de java
                                           // que aparece por defecto
     }
+    public void validarLetras (java.awt.event.KeyEvent evt){
+        int k=(int)evt.getKeyChar();
+            if ((k < 97 || k > 122) && (k<65 || k>90) && k!=20 && k!=8 && k!=32 && k!=127){
+                evt.setKeyChar((char)KeyEvent.VK_CLEAR);
+                JOptionPane.showMessageDialog(null,"Sólo debe ingresar letras","Error Datos",JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    public boolean esVacio (JTextField campo, String mensaje){
+            if (campo.getText().isEmpty()|| "Ingresa tu primer nombre".equals(campo.getText())|| campo.getText().equals("Ingresa tu primer apellido")||campo.getText().equals("tucorreo@gmail.com")|| campo.getText().equals("********"))
+               { JOptionPane.showMessageDialog(null, mensaje, "Error falta un dato", JOptionPane.ERROR_MESSAGE); 
+                 return true; 
+               }
+            else
+                 return false;
+        }  
+
+    public boolean validarEntero (String numero, String mensaje){
+        try{
+            int numero1 = Integer.parseInt(numero);
+            return true; 
+        }catch(NumberFormatException ex){
+            JOptionPane.showMessageDialog(null,mensaje,"Debe indicar un numero entero", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+    }
     
+    protected LocalDate validarFecha(JTextField fecha) {
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/uuuu").withResolverStyle(ResolverStyle.STRICT);
+        
+        LocalDate fechaValida = null;
+        while (fechaValida == null) {
+            try {
+                String fechaentrada = fecha.getText();
+                LocalDate fechaFinal = LocalDate.parse(fechaentrada, dtf);
+                LocalDate hoy = LocalDate.now();
+            if (fechaFinal.isBefore(hoy) || fechaFinal.isEqual(hoy)) {
+                fechaValida = fechaFinal;
+            } else {
+                JOptionPane.showMessageDialog(null,"La fecha debe ser hasta el dia de hoy.","Error al cargar fecha.", JOptionPane.ERROR_MESSAGE);
+            }
+            } catch (DateTimeParseException e) {
+                JOptionPane.showMessageDialog(null,"El formato de la fecha es incorrecto, recuerde que es 'dd/MM/aaaa'.","Error al cargar fecha.", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+        return fechaValida;
+    }
 }
